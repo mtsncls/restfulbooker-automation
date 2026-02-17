23734785020d8b827a7d55c1bbf38a7129feac88
Matias Alfonso <alfonsomatias@gmail.com>
2026-02-17 20:25:37 +0100

    Remove .idea from repo and update .gitignore

--- src/main/java/ui/pages/BasePage.java
+++ src/main/java/ui/pages/BasePage.java
@@ -1,4 +1,70 @@
 package ui.pages;
 
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class BasePage {
+
+    protected WebDriver driver;
+    private WebDriverWait wait;
+
+    public BasePage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    protected void navigateTo(String url) {
+        driver.get(url);
+    }
+
+    protected WebElement waitForElementToBeVisible(By locator) {
+        return wait.until(ExpectedConditions.visibilityOfElementLocated(locator));
+    }
+
+    protected WebElement waitForElementToBeClickable(By locator) {
+        return wait.until(ExpectedConditions.elementToBeClickable(locator));
+    }
+
+    protected boolean waitForInvisibility(By locator) {
+        return wait.until(ExpectedConditions.invisibilityOfElementLocated(locator));
+    }
+
+    protected void click(By locator) {
+        waitForElementToBeClickable(locator).click();
+    }
+
+    protected void type(By locator, String text) {
+        WebElement element = waitForElementToBeVisible(locator);
+        element.clear(); element.sendKeys(text);
+    }
+
+    protected String getText(By locator) {
+        return waitForElementToBeVisible(locator).getText();
+    }
+
+    protected boolean isDisplayed(By locator) {
+        try {
+            return waitForElementToBeVisible(locator).isDisplayed();
+        } catch (TimeoutException e) {
+            return false;
+        }
+    }
+
+    protected void jsClick(By locator) {
+        WebElement element = waitForElementToBeClickable(locator);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].click();", element);
+    }
+
+    protected void scrollIntoView(By locator) {
+        WebElement element = waitForElementToBeVisible(locator);
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
+    }
+
+    protected void pause(long millis) throws InterruptedException {
+        Thread.sleep(millis);
+    }
+
 }
